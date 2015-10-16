3b0b6da3753742e51c0fdbf4640cc7625b96e892
Eoin Molloy <eoinmolloy@outlook.com>
2015-10-16 15:59:37 +0100

    test

--- Translate/ViewController.swift
+++ Translate/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var translatedText: UITextView!
     
     //var data = NSMutableData()
+    // cgctdg
     
     override func viewDidLoad() {
         super.viewDidLoad()
